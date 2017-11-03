85d7dace3bf3ce7974a0af51e6fa26bdc6b5e3a3
Danyal <danyal@danyals-mbp.attlocal.net>
2017-11-03 18:18:29 -0500

    Skelaton of 4 tabs and controllers

---
+++ prayerApp/client/scripts/controllers/settings.controller.js
@@ -0,0 +1,7 @@
+import { Controller } from 'angular-ecmascript/module-helpers';
+ 
+export default class SettingsCtrl extends Controller {
+	
+}
+ 
+SettingsCtrl.$name = 'SettingsCtrl';
--- prayerApp/client/scripts/routes.js
+++ prayerApp/client/scripts/routes.js
@@ -1,6 +1,9 @@
 import { Config } from 'angular-ecmascript/module-helpers';
  
 import compassTemplateUrl from '../templates/compass.html';
+import timingTemplateUrl from '../templates/timing.html';
+import notificationsTemplateUrl from '../templates/notifications.html';
+import settingsTemplateUrl from '../templates/settings.html';
 import tabsTemplateUrl from '../templates/tabs.html';
  
 export default class RoutesConfig extends Config {
@@ -17,6 +20,33 @@ export default class RoutesConfig extends Config {
           'tab-compass': {
             templateUrl: compassTemplateUrl,
             controller: 'CompassCtrl as compass'
+          }
+        }
+      })
+      .state('tab.timing', {
+        url: '/timing',
+        views: {
+          'tab-timing': {
+            templateUrl: timingTemplateUrl,
+            controller: 'TimingCtrl as timing'
+          }
+        }
+      })
+      .state('tab.notifications', {
+        url: '/notifications',
+        views: {
+          'tab-notifications': {
+            templateUrl: notificationsTemplateUrl,
+            controller: 'NotificationsCtrl as notifications'
+          }
+        }
+      })
+      .state('tab.settings', {
+        url: '/settings',
+        views: {
+          'tab-settings': {
+            templateUrl: settingsTemplateUrl,
+            controller: 'SettingsCtrl as settings'
           }
         }
       });
